3e8b74c26fc033e6d4f456467d4b883c162b5327
Marius Mandal <mariusmandal@gmail.com>
2020-02-13 23:22:00 +0100

    Lagt til basisfilter

---
+++ TemplateEngine/TwigFilters.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UKMNorge\TemplateEngine;
+
+class TwigFilters {
+    /**
+     * TWIG-filter: |UKMpath
+     * Korrigerer path fra Symfony2-standard til current implementation
+     * 
+     * @param [type] $path
+     * @return void
+     */
+    function UKMpath( $path ) {
+        return str_replace(array('UKMDesignBundle',':'), array('','/'), $path );
+    }
+}
